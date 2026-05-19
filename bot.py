fb1358139dac05bb7327697403898b654c4cf767
Pedp4 WPBX4125BLF092411157 <wpbx4125blf092411157@pedp4.onmicrosoft.com>
2026-05-19 22:56:57 +0600

    fix bot deploy

--- bot.py
+++ bot.py
@@ -2,13 +2,17 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8947513136:AAGkBayU_qsgO1E1S7AyV-cnpRZwjRb0VFs
-")
+TOKEN = os.getenv("8947513136:AAGkBayU_qsgO1E1S7AyV-cnpRZwjRb0VFs")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is running 🚀")
+    await update.message.reply_text("Bot is running successfully 🚀")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+    print("Bot started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
